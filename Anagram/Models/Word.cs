e5932db25f2881719c3d0d4cae9d592c00aa59be
BrentHickman <BrentHickman@ymail.com>
2023-02-21 11:58:40 -0800

    Word third test passed
    Co-authored-by: James Provance <jb24x7@gmail.com>
    Co-authored-by: Charles Cookman <charlesbcookman@gmail.com>

--- Anagram/Models/Word.cs
+++ Anagram/Models/Word.cs
@@ -1,12 +1,28 @@
+using System;
 
 namespace Anagram.Models
 {
   public class Word
   {
     public string InputWord;
-    public Word(string newInput )
+    
+    public Word(string newInput)
     {
       InputWord = newInput;
+
+    }
+    public char[] checkWords(string s)
+    {
+      char[] ch = new char[s.Length];
+      for (int i = 0; i < s.Length; i++)
+      { 
+        ch[i] = s[i];
+      }
+      foreach (char c in ch)
+      {
+        Console.WriteLine(ch[0]);
+      }
+      return ch;
     }
   }
 }
