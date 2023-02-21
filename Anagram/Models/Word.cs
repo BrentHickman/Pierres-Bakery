9dd75f1f3649b503441482b5402438cc36aeafff
BrentHickman <BrentHickman@ymail.com>
2023-02-21 15:20:26 -0800

    adds array elements to char list
    Co-authored-by: James Provance <jb24x7@gmail.com>
    Co-authored-by: Charles Cookman <charlesbcookman@gmail.com>

--- Anagram/Models/Word.cs
+++ Anagram/Models/Word.cs
@@ -1,28 +1,39 @@
 using System;
+using System.Collections.Generic;
 
 namespace Anagram.Models
 {
   public class Word
   {
     public string InputWord;
-    
+    public char[] InputChars;
+    public static List<Char> dataList = new List<Char> { };
+
     public Word(string newInput)
     {
       InputWord = newInput;
-
+    }
+    public static void ClearAll()
+    {
+      dataList.Clear();
     }
     public char[] checkWords(string s)
     {
       char[] ch = new char[s.Length];
       for (int i = 0; i < s.Length; i++)
-      { 
-        ch[i] = s[i];
-      }
-      foreach (char c in ch)
       {
-        Console.WriteLine(ch[0]);
+        ch[i] = s[i];
+        dataList.Add(ch[i]);
+        Console.WriteLine(dataList[i]); 
       }
+      // sort array
+      //foreach for the array to add into charList
       return ch;
+    }
+    public static List<Char> GetAll()
+    {
+      Console.WriteLine(dataList);
+      return dataList;
     }
   }
 }
