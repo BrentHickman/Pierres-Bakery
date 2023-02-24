bd84fc931999b84960c10348ad57fb741d13a387
BrentHickman <brenthickman@ymail.com>
2023-02-24 10:10:00 -0800

    new testing directory is working

---
+++ Bakery.Tests/Model.Tests/Bread.Tests.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery.Models;
+
+namespace ShapeTracker.Tests
+{
+  [TestClass]
+  public class BreadTests
+  {
+    // Tests will go here!
+  }
+}
