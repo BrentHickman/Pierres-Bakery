2a43aa9d7e8e61a2ca3e0260bf72826b690a90ed
BrentHickman <brenthickman@ymail.com>
2023-02-24 11:36:49 -0800

    Bread passes test 3

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -5,9 +5,13 @@ namespace Bakery.Models;
 public class Bread
 {
   public int BreadQty { get; set; }
+  public int BreadOrderPrice { get; set; }
+  private int _loafprice;
   public Bread(int breadQtyOrdered)
   {
+    _loafprice = 5;
     BreadQty = breadQtyOrdered;
+    BreadOrderPrice = (breadQtyOrdered - (breadQtyOrdered/3))*_loafprice;
   }
 
 
